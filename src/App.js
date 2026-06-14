49876a9e74867481838a02f9ae326cf338956613
Pon-Ponovskiy <celoveka269@gmail.com>
2026-06-14 11:09:17 +0300

    14/06/2026

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
+import { useFormStatus } from 'react-dom';
 import './App.css';
 import {Users} from './components/Users/Users';
-
+import { useState, useEffect } from 'react';
 
 function App(){
 
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+  const [isInvites, setInvites] = useState([])
+  const onChangeSearchValue=(event) =>{
+      setSearchValue(event.target.value);
+    }
+  const onClickInvites=(id)=>{
+    console.log(id)
+  }
+
+
+  useEffect(() => {
+    try{
+        fetch('https://reqres.in/api/users', {
+          headers: {
+            'x-api-key': 'reqres_d20e984ba6ed41b887232b6feeadadb1'
+          }
+        })
+        .then(res => res.json())
+        .then(json => {
+          console.log(json)
+          console.log(json.data)
+          setUsers(json.data);
+        })
+        .catch((err) => {
+          console.log(err); 
+        })
+      }
+      finally{
+        setIsLoading(false)
+      }
+    }, []);
+    
   return(
-    <div className='App'>
-      <Users />
+    <div className='App'> 
+      <Users items={users}
+        isLoading={isLoading}
+        searchValue={searchValue}
+        onChangeValue={onChangeSearchValue}
+        isInvites={isInvites}
+        onClickInvites={onClickInvites}   
+        />
     </div>
   );
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Skeleton} from '../Skeletons/Skeletons';
 import {User} from './User';
 import Search from "../Icons/Search";
-export const Users = ({items, isLoading}) => {
+export const Users = ({items, isLoading, searchValue, onChangeValue}) => {
 return (
 <>
 <div className="search">
 <Search/>
-<input type="text" placeholder="Найти пользователя..."/>
+<input type="text" placeholder="Найти пользователя..." value={searchValue} onChange={onChangeValue}/>
 </div>
 {isLoading ? (
 <div className="skeleton-list">
@@ -17,10 +17,22 @@ return (
 </div>
 ) : (
 <ul className="users-list">
-<User/>
+    {
+        items
+        .filter(obj => {
+            const fullName = (obj.first_name + obj.last_name).toLowerCase();
+            if (fullName.includes(searchValue.toLowerCase()) || obj.email.toLowerCase().includes(searchValue.toLowerCase())) {
+                return true;
+            }
+            })
+        .map((users) => (<User first_name={users.first_name} last_name={users.last_name} email={users.email} id={users.id} avatar={users.avatar} />))
+        }
+
 </ul>
 )}
 <button className="send-invite-btn">Отправить приглашение</button>
 </>
 );
 };
+
+export default Users;
